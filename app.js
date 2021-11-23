3876936e2710f5d4aa5cc10be39719480af21a41
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2021-11-23 16:29:18 +0300

    update chatbot - integration with mongoDB

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const server = http.createServer(app)
-const {Server} = require('socket.io')
-const io = new Server(server)
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/index.html')
-})
+const Message = require("./Message");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/chatbox", (error) => {
+  if (!error) {
+    console.log("Connected Yippee..!!");
+  } else {
+    console.log("Error connecting to database.");
+  }
+});
 
-io.on('connection', (socket) =>{
-    console.log(`Client connected: ${socket.id}`)
-    
-    socket.on('send_message', (data) =>{
-        io.emit('receive_message', data)
-        console.log(data)
-    })
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-server.listen(3000, ()=>{
-    console.log("Server started on port 3000")
-})
+io.on("connection", (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+
+  // Get all messages from the database.
+  Message.find()
+    .sort({ createdAt: -1 })
+    .exec((err, messages) => {
+      if (err) return console.error(err);
+      console.log("messages from db: " + messages);
+      // Send messages to the user.
+      socket.emit("init", messages);
+    });
+
+  // Listen to connected users for a new message.
+  socket.on("message", (msg) => {
+    // Create a message.
+    const message = new Message({
+      content: msg.content,
+      from: msg.from,
+      to: msg.to,
+      createdAt: msg.time,
+    });
+
+    // Save the message to the database.
+    message.save((err) => {
+      if (err) return console.error(err);
+    });
+
+    // Notify all other users about a new message.
+    socket.broadcast.emit("push", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
